7a3253c6aee4b2d392c0b08541dee937da7993c8
Dustin Spicuzza <dustin@virtualroadside.com>
2020-02-11 04:54:56 -0500

    Format

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ if rtd_version not in ["stable", "latest"]:
 
 # -- Project information -----------------------------------------------------
 
-project = 'REV Color V3'
-copyright = '2020, RobotPy Development Team'
-author = 'RobotPy Development Team'
+project = "REV Color V3"
+copyright = "2020, RobotPy Development Team"
+author = "RobotPy Development Team"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -55,12 +55,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -76,7 +76,7 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # -- Custom Document processing ----------------------------------------------
 
